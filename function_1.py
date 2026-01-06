56faea62b01d14b32f3d7f5e13d386fab4136fc4
egoist945402376 <egoist945402376@gmail.com>
2026-01-06 19:31:25 +0000

    可以用/了

--- function_1.py
+++ function_1.py
@@ -13,8 +13,19 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 
 
 @bot.command()
+async def synccommands(ctx):
+    await bot.tree.sync()
+    await ctx.send("命令同步完成")
+
+
+@bot.hybrid_command()
 async def greet(ctx):
     await ctx.send("你说什么我都听不懂， 因为我只是一只奶牛猫。 我只听得懂我的主人跟我说“枣子过来”。")
+
+
+@bot.hybrid_command()
+async def add(ctx, a:int , b:int):
+    await ctx.send(a + b)
 
 
 bot.run(token)
